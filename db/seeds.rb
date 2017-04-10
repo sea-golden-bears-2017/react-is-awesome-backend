34db9573512a8d264371433ca4d0c46f97e003d0
Jordan Kamin <jordanakamin@gmail.com>
2017-04-10 15:46:38 -0700

    Add seeding for books

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+100.times do
+  Book.create(title: Faker::Book.title,
+    author: Faker::Book.author,
+    publisher: Faker::Book.publisher,
+    genre: Faker::Book.genre)
+end
+
+author = Faker::Book.author
+publisher = Faker::Book.publisher
+
+10.times do
+  Book.create(title: Faker::Book.title,
+    author: author,
+    publisher: publisher,
+    genre: Faker::Book.publisher)
+end
