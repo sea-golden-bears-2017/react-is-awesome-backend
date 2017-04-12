f23c134d2cb4c56c55831dfdab67dbd7f5d0f350
Jordan Kamin <jordanakamin@gmail.com>
2017-04-12 10:19:58 -0700

    Adds UsersController and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :books, except: :update
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :update]
 end
