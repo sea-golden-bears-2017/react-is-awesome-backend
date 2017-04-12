40794b174ca3716b31ff20bcd523910960370e89
Jordan Kamin <jordanakamin@gmail.com>
2017-04-12 11:56:20 -0700

    namespaces UsersController#create response under user: key

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     user.save
-    render json: user.name
+    render json: {name: user.name}
   end
 
   private
