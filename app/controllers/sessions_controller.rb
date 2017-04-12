b90541219c36e43c51bdce89f7d31b370c1a3c7f
Jordan Kamin <jordanakamin@gmail.com>
2017-04-12 14:15:24 -0700

    Adds SessionsController.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,16 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_by(name: session_params[:name])
+    if user.authenticate(session_params[:password])
+      session[:id] = user.id
+    end
   end
   def destroy
+  end
+
+  private
+
+  def session_params
+    params.permit(:name, :password)
   end
 end
