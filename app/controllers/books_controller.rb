1a9a2d2c1b376657cf14a54e798ff78e945392a1
Jordan Kamin <jordanakamin@gmail.com>
2017-04-17 12:04:25 -0700

    adds route for book index with user id

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
   def index
-    render json: Book.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      render json: user.books
+    else
+      render json: Book.all
+    end
   end
 
   def show
--- app/controllers/books_users_controller.rb
+++ app/controllers/books_users_controller.rb
@@ -1,6 +1,6 @@
 class BooksUsersController < ApplicationController
-  def index
-    @books = Book.where(user_id: params[:user_id])
-    render json: @books
-  end
+  # def index
+  #   @books = Book.where(user_id: params[:user_id])
+  #   render json: @books
+  # end
 end
