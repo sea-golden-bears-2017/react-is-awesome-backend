40811636a610eee4c3a2b3f70586ad9994e5b0ab
Anil Kulkarni <anil.kulkarni@outlook.com>
2017-04-17 14:32:31 -0700

    Add auth helpers to simplify controller creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
+class UnauthorizedError < StandardError
+end
+
 class ApplicationController < ActionController::API
+  def authorize!
+    if !authorize
+      raise UnauthorizedError
+    end
+  end
+
+  def authorize
+    return params[:user_id] == session[:user_id] && current_user
+  end
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  rescue_from UnauthorizedError do |exception|
+    error = 'Unauthorized access. Please log in and try again'
+    render json: {error: error }, status: 403
+  end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,8 @@
 class BooksController < ApplicationController
   def index
     if params[:user_id]
-      if session[:user_id] == params[:user_id]
-        user = User.find(params[:user_id])
-        render json: user.books
-      else
-        render json: {error: 'Unauthorized access. Please log in and try again.'}, status: 403
-      end
+      authorize!
+      render json: current_user.books
     else
       render json: Book.all
     end
