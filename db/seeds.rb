354d4bae76c974ef5222a6772e7e16147a60e0bc
Anil Kulkarni <anil.kulkarni@outlook.com>
2017-04-21 21:50:48 -0700

    seed users with friends and books

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require 'faker'
-100.times do
+1000.times do
   Book.create(title: Faker::Book.title,
     author: Faker::Book.author,
     publisher: Faker::Book.publisher,
@@ -9,7 +9,7 @@ end
 author = Faker::Book.author
 publisher = Faker::Book.publisher
 
-10.times do
+100.times do
   Book.create(title: Faker::Book.title,
     author: author,
     publisher: publisher,
@@ -22,4 +22,20 @@ end
 
 10.times do
   Food.create(name: Faker::Food.spice, unit: Faker::Food.measurement)
+end
+
+
+100.times do
+  User.create(name: Faker::Internet.user_name, password: Faker::Internet.password)
+end
+
+10.times do
+  user = User.create(name: Faker::Internet.user_name, password: Faker::Internet.password)
+  5.times do
+    user.friends << User.create(name: Faker::Internet.user_name, password: Faker::Internet.password)
+    user.books << Book.create(title: Faker::Book.title,
+      author: author,
+      publisher: publisher,
+      genre: Faker::Book.publisher)
+  end
 end
